43e5e2d29a45df595c3a73d6910228665c9fe7bd
root <root@localhost>
2023-09-05 18:45:21 +0000

    convert basic functionality from task_manager

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+  root "tasks#index"
+
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
